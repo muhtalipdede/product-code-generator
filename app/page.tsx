f6a532e97f850797395f1725cf16b331820d5e0f
Muhtalip Dede <muhtalipdede@gmail.com>
2024-04-09 22:20:09 +0300

    Refactor CSS and remove unused code

--- app/page.tsx
+++ app/page.tsx
@@ -179,14 +179,14 @@ export default function CSVProcessor() {
 
   return (
     <main>
-      <div className="flex flex-row gap-4 px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8 bg-gray-100 rounded-lg shadow-lg p-4">
+      <div className="flex flex-row gap-4 px-4 py-6 mx-auto sm:px-6 lg:px-8 p-4">
         <div className="flex flex-col gap-2">
           <label className="text-lg font-bold">Family List</label>
           <input
             type="file"
             onChange={handleFamilyList}
             accept=".csv"
-            className="w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            className="w-full text-sm  border border-gray-300 cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
           />
           <p className="text-sm text-gray-500">
             Only CSV files are supported
@@ -200,7 +200,7 @@ export default function CSVProcessor() {
             type="file"
             onChange={handleAttributeList}
             accept=".csv"
-            className="w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            className="w-full text-sm  border border-gray-300 cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
           />
           <p className="text-sm text-gray-500">
             Only CSV files are supported
@@ -210,8 +210,8 @@ export default function CSVProcessor() {
             </button>
           </p>
         </div>
-        <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-          <h1 className="text-3xl font-bold text-gray-900">
+        <div className="mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold ">
             Product Code Generator
           </h1>
           <p className="text-sm text-gray-500">
@@ -229,7 +229,7 @@ export default function CSVProcessor() {
         </div>
       </div>
 
-      {errorList.length > 0 && <div className="flex flex-col gap-4 px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8 bg-red-100 rounded-lg shadow-lg p-4">
+      {errorList.length > 0 && <div className="flex flex-col gap-4 px-4 py-6 mx-auto sm:px-6 lg:px-8 bg-red-100 p-4">
         {(
           errorList.map((error: any, index: any) => (
             <p className="text-red-500" key={index}>{error}</p>
@@ -237,7 +237,7 @@ export default function CSVProcessor() {
         )}
       </div>}
 
-      <div className="flex flex-row gap-4 px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8 bg-gray-100 rounded-lg shadow-lg p-4">
+      <div className="flex flex-row gap-4 px-4 py-6 mx-auto sm:px-6 lg:px-8 p-4">
         <button onClick={handleGenerateProductList} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Generate Product List
         </button>
@@ -251,7 +251,7 @@ export default function CSVProcessor() {
           Show Duplicate Product Codes
         </button>}
       </div>
-      <div className="flex flex-row gap-4 px-4 py-6 mx-auto max-w-7xl sm:px-6 lg:px-8 bg-gray-100 rounded-lg shadow-lg p-4 overflow-x-auto">
+      <div className="flex flex-row gap-4 px-4 py-6 mx-auto sm:px-6 lg:px-8 p-4 overflow-x-auto">
         {productList.length > 0 && (
           <table className="table min-w-full divide-y divide-gray-200 border border-gray-200 rounded shadow bg-white border-collapse text-xs">
             <thead className="bg-gray-50">
@@ -317,7 +317,7 @@ export default function CSVProcessor() {
               <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
             </div>
             <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-            <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+            <div className="inline-block align-bottom bg-white text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="sm:flex sm:items-start max-h-96 overflow-y-auto">
                   <button onClick={() => setShowDuplicateProductCodesModal(false)} className="absolute top-0 right-0 p-2 m-2 text-gray-500 hover:text-gray-700 focus:outline-none">
@@ -326,7 +326,7 @@ export default function CSVProcessor() {
                     </svg>
                   </button>
                   <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <h3 className="text-lg font-bold leading-6 text-gray-900">Duplicate Product Codes</h3>
+                    <h3 className="text-lg font-bold leading-6 ">Duplicate Product Codes</h3>
                     <div className="mt-2">
                       <p className="text-sm text-gray-500">
                         {duplicateProductCodes.map((productCode, index) => (
