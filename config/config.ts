a896d09b54e0c257e5479a8e035b1d41934ab1fe
Muhtalip Dede <muhtalipdede@gmail.com>
2024-06-10 22:48:47 +0300

    refactor: update API_URL in config.ts

--- config/config.ts
+++ config/config.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost:3001/api';
-// export const API_URL = 'https://product-code-generator.vercel.app/api';
+// export const API_URL = 'http://localhost:3000/api';
+export const API_URL = 'https://product-code-generator.vercel.app/api';
 
